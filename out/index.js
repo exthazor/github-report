b0b336147047783c5ff9069563fd9b514d415496
Rhitam Dutta <rhitamdutta1996@gmail.com>
2020-12-25 19:18:00 +0530

    Added final UI changes and simplified code

--- out/index.js
+++ out/index.js
@@ -10,10 +10,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var _ = __importStar(require("lodash"));
 var GithubApiService_1 = require("./GithubApiService");
 var svc = new GithubApiService_1.GithubApiService();
-svc.getUserInfo('exthazor', function (user) {
-    svc.getRepos('exthazor', function (repos) {
-        var sortedRepos = _.sortBy(repos, function (repo) { return repo.forkCount; });
-        user.repos = sortedRepos;
+svc.getUserInfo('aSquare14', function (user) {
+    svc.getRepos('aSquare14', function (repos) {
+        var sortedRepos = _.sortBy(repos, [function (repo) { return repo.forkCount; }]).reverse();
+        user.repos = _.take(sortedRepos, 5);
         console.log(user);
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,19 @@ import { Repo } from './Repo'
 import { GithubApiService } from './GithubApiService'
 
 let svc = new GithubApiService();
-svc.getUserInfo('exthazor', (user:User) => {
-    svc.getRepos('exthazor', (repos: Repo[]) => {
-        let sortedRepos = _.sortBy(repos, (repo:Repo) => repo.forkCount * -1)
-        user.repos = sortedRepos
-        console.log(user)
+if(process.argv.length < 3){
+    console.log("Please enter the required Github username.")
+}
+else{
+    let username = process.argv[2]
+    svc.getUserInfo('username', (user:User) => {
+        svc.getRepos('username', (repos: Repo[]) => {
+            let sortedRepos = _.sortBy(repos, [(repo:Repo) => repo.forkCount ]).reverse()
+            user.repos = _.take(sortedRepos, 5)
+            console.log(user)
+        })
+        
     })
-    
-})
+}
 
 
